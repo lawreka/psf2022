01003eb7527a40e161535289fa4bd830f0b2b093
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2022-03-06 20:10:54 +0100

    fix: desktop style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
 const DOCUMENT = "/opencall.pdf";
 
 const App = () => {
+    const [height, setHeight] = useState(700);
+
+    const resize = () => {
+        const windowHeight = window.innerHeight;
+        setHeight(windowHeight);
+    }
+
+    useEffect(() => {
+        window.addEventListener('load', resize);
+        window.addEventListener('resize', resize);
+        return () => {
+            window.removeEventListener('load', resize);
+            window.removeEventListener('resize', resize);
+        }
+    }, []);
+
     return (
         <div className="App">
             <iframe
                 title="PDF"
                 src={`/pdfjs-2.5.207-es5-dist/web/viewer.html?file=${DOCUMENT}`}
                 width="100%"
-                height="700px"
+                height={height}
             ></iframe>
         </div>
     )
