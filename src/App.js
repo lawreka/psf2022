8639c9a56c634725582a35d8f5beff9a73d3382c
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2022-07-19 09:43:49 +0200

    remove pdf add Coming Soon

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import { useState, useEffect } from 'react';
 import './App.css';
 
-const DOCUMENT = "/ProjectSpaceFestival2022ProgramCalendar.pdf";
-
 const App = () => {
-    const [height, setHeight] = useState(700);
-
-    const resize = () => {
-        const windowHeight = window.innerHeight;
-        setHeight(windowHeight);
-    }
-
-    useEffect(() => {
-        window.addEventListener('load', resize);
-        window.addEventListener('resize', resize);
-        return () => {
-            window.removeEventListener('load', resize);
-            window.removeEventListener('resize', resize);
-        }
-    }, []);
-
     return (
         <div className="App">
-            <iframe
-                title="PDF"
-                src={`/pdfjs-2.5.207-es5-dist/web/viewer.html?file=${DOCUMENT}`}
-                width="100%"
-                height={height}
-            ></iframe>
+            <div style={{ position: "fixed", top: "0px", left: "0px", width: "100%", height: "100%", zIndex: "100", backgroundColor: "#ff3d00", display: "flex", justifyContent: "center", alignItems: "center", fontFamily: 'Termina', fontSize: '2em' }}>
+                Coming Soon
+            </div>
         </div>
     )
 }
